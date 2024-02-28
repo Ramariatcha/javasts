dc3ea2fc82bfeb8a6c2afd155961e8f9b1804a89
Joao Costa <joao.costa@icarotech.com>
2024-02-28 18:57:12 -0300

    adicionado order

--- src/main/java/com/example/demo/entities/User.java
+++ src/main/java/com/example/demo/entities/User.java
@@ -1,11 +1,13 @@
 package com.example.demo.entities;
 
 import java.io.Serializable;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.GenerationType;
 
@@ -24,6 +26,9 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders; 
 	
 	public User() {
 		
@@ -92,6 +97,12 @@ public class User implements Serializable{
 			return false;
 		User other = (User) obj;
 		return id == other.id;
+	}
+
+
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 	
 	
