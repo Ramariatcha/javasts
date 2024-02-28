22791405079ad32fcf1b16a3e39e06bc14676bd4
Joao Costa <joao.costa@icarotech.com>
2024-02-28 17:18:58 -0300

    adicionada rota findAll e findById

--- src/main/java/com/example/demo/resources/UserResources.java
+++ src/main/java/com/example/demo/resources/UserResources.java
@@ -1,18 +1,35 @@
 package com.example.demo.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entities.User;
+import com.example.demo.services.UserService;
+
+
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResources {
+
+	@Autowired 
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User((long) 1, "Maria", "maria@gmail.com", "1123456578989", "senha");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
